3fccb14615ca357a02482114de670be61caadbb4
eevangeline <s10239693@connect.np.edu.sg>
2022-08-03 12:37:46 +0800

    profit and loss

--- profit_loss.py
+++ profit_loss.py
@@ -10,3 +10,17 @@ with fp.open(mode="r", encoding="UTF-8" ) as file:
     for row in reader:
         list_profitnloss.append(float(row[4])*exchange_rate)
         list_days.append(int(row[0]))
+def profitnloss():
+    first = list_profitnloss[0]
+    deficit = False
+    days_deficit = []
+    for x in range(len(list_profitnloss)-1):
+        diff = list_profitnloss[x+1]-first
+        if diff <0:
+            days_deficit.append(f"[PROFIT DEFICIT] DAY: {list_days[x+1]}, AMOUNT: SGD{round(abs(diff),1)}")
+            deficit = True
+        first = list_profitnloss[x+1]
+    if deficit == False:
+        days_deficit.append(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY.")
+    return days_deficit
+print(profitnloss())
