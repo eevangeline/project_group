4ef43a233a07a02ea219d2a0ba9f2ad67a9260a1
eevangeline <s10239693@connect.np.edu.sg>
2022-08-03 21:31:43 +0800

    comments

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ try:
     from overheads import overheads
     from pathlib import Path
     import csv
+#exception handling for import error in case we change name of keyword or have typos
 except ImportError:
     print(f"Import keyword not found! Please check keyword.")
 fp = Path.cwd()/"summary_report.txt"
+#creating text file to write summary report into
 fp.touch()
+#opening file to write summary report using write mode
 print(fp.exists())
 for data in coh():
     word = data
